faf9de4dca4f7cbd26dcc7f44f533c2da8cdbe87
Pierre Kestener <pierre.kestener@cea.fr>
2021-11-27 19:44:56 +0100

    Update cmake

--- test_mul.py
+++ test_mul.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import gpu_library
 import numpy as np
 import time
@@ -12,10 +14,10 @@ factor = 3.0
 t0 = time.time()
 for _ in range(runs):
     gpu_library.multiply_with_scalar(arr1, factor)
-print("gpu time: " + str(time.time()-t0))
+print("gpu time: {}".format(time.time()-t0))
 t0 = time.time()
 for _ in range(runs):
     arr2 = arr2 * factor
-print("cpu time: " + str(time.time()-t0))
+print("cpu time: {}".format(time.time()-t0))
 
-print("results match: " + str(np.allclose(arr1,arr2)))
+print("results match: {}".format(np.allclose(arr1,arr2)))
